030b284d7bd3fc241b6623cf7bed201d7956ca3f
GP2015 <adamcunningham2015@hotmail.com>
2025-10-25 19:22:24 +0100

    Added basic ROM

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+mod bus;
+mod cpu;
+mod rom;
+mod state;
+
+use crate::bus::Bus;
+use crate::cpu::CPU;
+use crate::rom::ROM;
+use crate::state::State;
+use anyhow::{Result, anyhow};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    program_path: String,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let state = State::new();
+
+    let rw_line = Bus::new(false);
+    let data_bus = Bus::<u8>::new(0);
+    let addr_bus = Bus::<u16>::new(0);
+
+    let rom = ROM::new(&args.program_path)?;
+    let cpu = CPU::new(&rom)?;
+
+    Ok(())
 }
