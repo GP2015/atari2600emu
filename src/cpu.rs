293eb2c888d642a1fab3380d8cc6b86895784bf3
GP2015 <adamcunningham2015@hotmail.com>
2025-10-26 12:33:20 +0000

    Added CPU default values

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,8 +1,13 @@
 use crate::mapper::Mapper;
 use anyhow::Result;
 
-const PC_START_ADDR_HIGH_BYTE: usize = 0xFFFC;
-const PC_START_ADDR_LOW_BYTE: usize = 0xFFFD;
+const PC_RESET_ADDR_HIGH_BYTE: usize = 0xFFFC;
+const PC_RESET_ADDR_LOW_BYTE: usize = 0xFFFD;
+
+const ACCUMULATOR_RESET_VALUE: u8 = 0x00;
+const X_REGISTER_RESET_VALUE: u8 = 0x00;
+const Y_REGISTER_RESET_VALUE: u8 = 0x00;
+const STATUS_REGISTER_RESET_VALUE: u8 = 0x00;
 const STACK_POINTER_RESET_VALUE: u8 = 0xFD;
 
 pub struct CPU {
@@ -16,17 +21,17 @@ pub struct CPU {
 
 impl CPU {
     pub fn new<T: Mapper>(cart: &mut T) -> Result<Self> {
-        let pc_high_byte = cart.read(PC_START_ADDR_HIGH_BYTE)?;
-        let pc_low_byte = cart.read(PC_START_ADDR_LOW_BYTE)?;
+        let pc_high_byte = cart.read(PC_RESET_ADDR_HIGH_BYTE)?;
+        let pc_low_byte = cart.read(PC_RESET_ADDR_LOW_BYTE)?;
 
         let pc_start_addr = ((pc_high_byte as u16) << 8) | (pc_low_byte as u16);
 
         Ok(Self {
             program_counter: pc_start_addr,
-            accumulator: 0,
-            x_register: 0,
-            y_register: 0,
-            status_register: 0,
+            accumulator: ACCUMULATOR_RESET_VALUE,
+            x_register: X_REGISTER_RESET_VALUE,
+            y_register: Y_REGISTER_RESET_VALUE,
+            status_register: STATUS_REGISTER_RESET_VALUE,
             stack_pointer: STACK_POINTER_RESET_VALUE,
         })
     }
