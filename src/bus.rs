a14960a6add67e28954557ece0336e2625594eb6
GP2015 <adamcunningham2015@hotmail.com>
2025-10-26 12:17:44 +0000

    Added bus unit tests

--- src/bus.rs
+++ src/bus.rs
@@ -2,7 +2,10 @@ pub struct Bus<T: Copy> {
     value: T,
 }
 
-impl<T: Copy> Bus<T> {
+impl<T> Bus<T>
+where
+    T: Copy,
+{
     pub fn new(initial_value: T) -> Self {
         Self {
             value: initial_value,
@@ -15,5 +18,26 @@ impl<T: Copy> Bus<T> {
 
     pub fn set(&mut self, new_value: T) {
         self.value = new_value;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_u8_bus() {
+        let mut bus = Bus::<u8>::new(6);
+        assert_eq!(bus.get(), 6 as u8);
+        bus.set(7);
+        assert_eq!(bus.get(), 7 as u8);
+    }
+
+    #[test]
+    fn test_bool_bus() {
+        let mut bus = Bus::new(true);
+        assert_eq!(bus.get(), true);
+        bus.set(false);
+        assert_eq!(bus.get(), false);
     }
 }
